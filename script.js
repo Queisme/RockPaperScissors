ee4ebedc8b1df400ef8e9895962f63a4ab4fdef2
Queisme <queiskey@gmail.com>
2024-07-23 14:16:23 -0500

    Rock, Paper, Scissors

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-let computerScore = '';
-let playerScore = '';
-let computerPick = '';
+let computerScore = 0;
+let humanScore = 0;
+let computerChoice = '';
+let humanChoice = '';
 const choices = ['rock', 'paper', 'scissors'];
 
 const getComputerChoice = () => {
-  computerPick = choices[Math.floor(Math.random() * choices.length)];
-  console.log(`Computer picks: ${computerPick}`);
+  computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  console.log(`Computer picks: ${computerChoice}`);
 };
 
-const getHumanChoice = prompt(
-  'Pick rock, paper, or scissors: '.toLowerCase(),
-  getComputerChoice()
-);
-console.log(`You chose: ${getHumanChoice}`);
+const getHumanChoice = () => {
+  humanChoice = prompt('Pick rock, paper, or scissors: '.toLowerCase());
+  console.log(`You picked: ${humanChoice}`);
+  getComputerChoice();
+  compare();
+};
 
 const compare = () => {
-  if (getHumanChoice === computerPick) {
+  if (humanChoice === computerChoice) {
     console.log('Tie.');
   } else if (
-    (getHumanChoice === 'rock' && computerPick === 'scissors') ||
-    (getHumanChoice === 'paper' && computerPick === 'rock') ||
-    (getHumanChoice === 'scissors' && computerPick === 'paper')
+    (humanChoice === 'rock' && computerChoice === 'scissors') ||
+    (humanChoice === 'paper' && computerChoice === 'rock') ||
+    (humanChoice === 'scissors' && computerChoice === 'paper')
   ) {
     console.log('You win.');
+    humanScore += 1;
   } else {
     console.log('You Lose.');
+    computerScore += 1;
   }
+  console.log(`Your score: ${humanScore}`);
+  console.log(`Computer's Score: ${computerScore}`);
 };
 
-compare();
+const playRound = () => {
+  humanScore > computerScore ? 'Winner! Winner!' : 'Loser! Loser';
+};
+
+getHumanChoice();
