df60c1df9ef875a0be58c233d78df03a61a57fe8
Queisme <queiskey@gmail.com>
2024-07-23 11:41:55 -0500

    Step 3 - done

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-let playerPick = '';
+let computerScore = '';
+let playerScore = '';
 let computerPick = '';
 const choices = ['rock', 'paper', 'scissors'];
 
 const getComputerChoice = () => {
   computerPick = choices[Math.floor(Math.random() * choices.length)];
+  console.log(`Computer picks: ${computerPick}`);
 };
 
+const getHumanChoice = prompt(
+  'Pick rock, paper, or scissors: '.toLowerCase(),
+  getComputerChoice()
+);
+console.log(`You chose: ${getHumanChoice}`);
+
 const compare = () => {
-  if (playerPick === computerPick) {
+  if (getHumanChoice === computerPick) {
     console.log('Tie.');
   } else if (
-    (playerPick === 'rock' && computerPick === 'scissors') ||
-    (playerPick === 'paper' && computerPick === 'rock') ||
-    (playerPick === 'scissors' && computerPick === 'paper')
+    (getHumanChoice === 'rock' && computerPick === 'scissors') ||
+    (getHumanChoice === 'paper' && computerPick === 'rock') ||
+    (getHumanChoice === 'scissors' && computerPick === 'paper')
   ) {
     console.log('You win.');
   } else {
     console.log('You Lose.');
   }
 };
+
+compare();
